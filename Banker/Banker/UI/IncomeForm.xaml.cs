74a67db33188e8f77d3d5b8aba9cb0a682cd2d1a
Quyen Truong <quyen.truong0202@gmail.com>
2018-07-06 15:30:31 -0700

    Income theme done

--- Banker/Banker/UI/IncomeForm.xaml.cs
+++ Banker/Banker/UI/IncomeForm.xaml.cs
@@ -1,16 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
 using Banker.Database;
 
 namespace Banker
@@ -18,7 +8,7 @@ namespace Banker
     /// <summary>
     /// Interaction logic for IncomeForm.xaml
     /// </summary>
-    public partial class IncomeForm : Window
+    public partial class IncomeForm
     {
         private readonly int _userId;
         private readonly SqlConnect _sql;
--- Banker/Banker/UI/MainWindow.xaml.cs
+++ Banker/Banker/UI/MainWindow.xaml.cs
@@ -6,7 +6,7 @@ namespace Banker
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow
     {
         private readonly SqlConnect _sql;
         public MainWindow()
