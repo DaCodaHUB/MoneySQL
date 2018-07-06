a990b4f35053f20cf2693d049a0cb34c2cfd9999
DESKTOP-REC2E49\lewin <dannylpov@gmail.com>
2018-07-06 01:25:36 -0700

    Still need to add chart control

---
+++ Banker/Banker/IncomeForm.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Banker
+{
+    /// <summary>
+    /// Interaction logic for IncomeForm.xaml
+    /// </summary>
+    public partial class IncomeForm : Window
+    {
+        public IncomeForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
